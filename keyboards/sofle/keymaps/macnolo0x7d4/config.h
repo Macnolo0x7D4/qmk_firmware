91e1aa8ba11e3e65505db25f56278f6d06a4841e
Manuel Díaz <yosoymacnolo@gmail.com>
2025-04-12 13:18:27 -0600

    feat: port my sofle keymap

---
+++ keyboards/sofle/keymaps/macnolo0x7d4/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define ENCODER_DIRECTION_FLIP
+
+#undef ENCODER_RESOLUTION
+#define ENCODER_RESOLUTION 4
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
---
+++ keyboards/sofle/keymaps/macnolo0x7d4/keymap.c
@@ -0,0 +1,416 @@
+#include <stdint.h>
+#include QMK_KEYBOARD_H
+
+enum sofle_layers {
+  /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
+
+enum custom_keycodes {
+  KC_QWERTY = SAFE_RANGE,
+  KC_LOWER,
+  KC_RAISE,
+  KC_ADJUST,
+  KC_PRVDESK,
+  KC_NXTDESK,
+  KC_SC1,
+  KC_SC2,
+  KC_SC3,
+  KC_SC4,
+  KC_SC5,
+  KC_SC6,
+  KC_SC7,
+  KC_SC8,
+  KC_SC9,
+  KC_SC10
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/*
+ * QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[_QWERTY] = LAYOUT(
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
+  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    XXXXXXX, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                 KC_LGUI,KC_LALT,KC_LCTL, KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_RGUI, KC_RALT, KC_RCTL
+),
+/* LOWER
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * | Shift|  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  | Shift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ *
+ */
+[_LOWER] = LAYOUT(
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
+  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
+                       _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+),
+/* RAISE
+ * ,----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Esc  | Ins  | Pscr | Menu |      |      |                    |      | PWrd |  Up  | NWrd | DLine| Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  | LAt  | LCtl |LShift|      | Caps |-------.    ,-------|      | Left | Down | Rigth|  Del | Bspc |
+ * |------+------+------+------+------+------|  MUTE  |    |       |------+------+------+------+------+------|
+ * |Shift | Undo |  Cut | Copy | Paste|      |-------|    |-------|      | LStr |      | LEnd |      | Shift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+[_RAISE] = LAYOUT(
+  _______, KC_SC1, KC_SC2, KC_SC3 , KC_SC4 , KC_SC5,                           KC_SC6,  KC_SC7  , KC_SC8,  KC_SC9 ,  KC_SC10 ,_______,
+  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, XXXXXXX,   KC_UP, XXXXXXX,XXXXXXX, KC_BSPC,
+  _______, KC_PRVDESK,  KC_NXTDESK,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______,
+                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+),
+/* ADJUST
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | QK_BOOT|      |QWERTY|    |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |MACWIN|      |      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
+ * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+  [_ADJUST] = LAYOUT(
+  XXXXXXX , XXXXXXX,  XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  QK_BOOT  , XXXXXXX,KC_QWERTY,XXXXXXX, XXXXXXX,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX , XXXXXXX,CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+                   _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+  )
+};
+
+#ifdef OLED_ENABLE
+
+static void render_logo(void) {
+    static const char PROGMEM raw_logo[] = {
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,240, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,240,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,255,  0,  0,  0,135,135,199,199,231,231,247,119,127, 63, 63, 63, 31, 31, 15, 15,  7,  0,  0,255,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,255,  0,  0,  0,225,225,227,227,231,231,239,238,254,252,252,252,248,248,240,240,224,  0,  0,255,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 15,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    };
+
+    oled_write_raw_P(raw_logo, sizeof(raw_logo));
+}
+
+static void print_status_narrow(void) {
+    // Print current mode
+    oled_write_P(PSTR("\n\n"), false);
+    oled_write_ln_P(PSTR("MODE"), false);
+    oled_write_ln_P(PSTR(""), false);
+    if (keymap_config.swap_lctl_lgui) {
+        oled_write_ln_P(PSTR("MAC"), false);
+    } else {
+        oled_write_ln_P(PSTR("WIN"), false);
+    }
+
+    switch (get_highest_layer(default_layer_state)) {
+        case _QWERTY:
+            oled_write_ln_P(PSTR("Qwrt"), false);
+            break;
+        default:
+            oled_write_P(PSTR("Undef"), false);
+    }
+    oled_write_P(PSTR("\n\n"), false);
+
+    // Print current layer
+    oled_write_ln_P(PSTR("LAYER"), false);
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write_P(PSTR("Base\n"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("Raise"), false);
+            break;
+        case _LOWER:
+            oled_write_P(PSTR("Lower"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("Adj\n"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undef"), false);
+    }
+
+    oled_write_P(PSTR("\n\n"), false);
+    led_t led_usb_state = host_keyboard_led_state();
+    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
+    }
+    return rotation;
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        print_status_narrow();
+    } else {
+        render_logo();
+    }
+    return false;
+}
+
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+    case KC_LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+    case KC_RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+    case KC_ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+    case KC_COPY:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_code(KC_C);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_code(KC_C);
+      }
+      return false;
+    case KC_PASTE:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_code(KC_V);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_code(KC_V);
+      }
+      return false;
+    case KC_CUT:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_code(KC_X);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_code(KC_X);
+      }
+      return false;
+      break;
+    case KC_UNDO:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_code(KC_Z);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_code(KC_Z);
+      }
+      return false;
+    case KC_NXTDESK:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LGUI));
+        register_code(KC_RGHT);
+      } else {
+        unregister_mods(mod_config(MOD_LGUI));
+        unregister_code(KC_RGHT);
+      }
+      return false;
+    case KC_PRVDESK:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LGUI));
+        register_code(KC_LEFT);
+      } else {
+        unregister_mods(mod_config(MOD_LGUI));
+        unregister_code(KC_LEFT);
+      }
+      return false;
+    case KC_SC1:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_1);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_1);
+      }
+      return false;
+    case KC_SC2:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_2);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_2);
+      }
+      return false;
+    case KC_SC3:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_3);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_3);
+      }
+      return false;
+    case KC_SC4:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_4);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_4);
+      }
+      return false;
+    case KC_SC5:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_5);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_5);
+      }
+      return false;
+    case KC_SC6:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_6);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_6);
+      }
+      return false;
+    case KC_SC7:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_7);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_7);
+      }
+      return false;
+    case KC_SC8:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_8);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_8);
+      }
+      return false;
+    case KC_SC9:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_9);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_9);
+      }
+      return false;
+    case KC_SC10:
+      if (record->event.pressed) {
+        register_mods(mod_config(MOD_LCTL));
+        register_mods(mod_config(MOD_LALT));
+        register_code(KC_9);
+      } else {
+        unregister_mods(mod_config(MOD_LCTL));
+        unregister_mods(mod_config(MOD_LALT));
+        unregister_code(KC_9);
+      }
+      return false;
+  }
+  return true;
+}
+
+#ifdef ENCODER_ENABLE
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+    return true;
+}
+
+#endif
